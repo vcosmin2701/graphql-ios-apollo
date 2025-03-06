8099eece2232bd21c6902d0dd07da84ec57a2780
vcosmin2701 <v.cosmin2717@gmail.com>
2025-03-06 13:59:31 +0200

    Add RocketReserverAPI to project via codegen

---
+++ starter/RocketReserverAPI/Sources/Schema/SchemaMetadata.graphql.swift
@@ -0,0 +1,33 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public protocol SelectionSet: ApolloAPI.SelectionSet & ApolloAPI.RootSelectionSet
+where Schema == RocketReserverAPI.SchemaMetadata {}
+
+public protocol InlineFragment: ApolloAPI.SelectionSet & ApolloAPI.InlineFragment
+where Schema == RocketReserverAPI.SchemaMetadata {}
+
+public protocol MutableSelectionSet: ApolloAPI.MutableRootSelectionSet
+where Schema == RocketReserverAPI.SchemaMetadata {}
+
+public protocol MutableInlineFragment: ApolloAPI.MutableSelectionSet & ApolloAPI.InlineFragment
+where Schema == RocketReserverAPI.SchemaMetadata {}
+
+public enum SchemaMetadata: ApolloAPI.SchemaMetadata {
+  public static let configuration: any ApolloAPI.SchemaConfiguration.Type = SchemaConfiguration.self
+
+  public static func objectType(forTypename typename: String) -> ApolloAPI.Object? {
+    switch typename {
+    case "Launch": return RocketReserverAPI.Objects.Launch
+    case "LaunchConnection": return RocketReserverAPI.Objects.LaunchConnection
+    case "Query": return RocketReserverAPI.Objects.Query
+    default: return nil
+    }
+  }
+}
+
+public enum Objects {}
+public enum Interfaces {}
+public enum Unions {}
